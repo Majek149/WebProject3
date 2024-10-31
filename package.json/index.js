a4635ac57305a3c9d31ee6cfca7014c546c7a4ad
Kehinde M.S <majek149@gmail.com>
2024-10-31 04:18:54 -0700

    Add files via upload

---
+++ package.json/index.js
@@ -0,0 +1,9 @@
+const ContainerEl = document.getElementById("container");
+const formEl = document.getElementById("form");
+
+const btnEl = document.querySelector("btn");
+const resultEl = document.querySelector("result");
+
+showBtn.addEventListener("click", () => {
+dialogElem.showModal();
+});
